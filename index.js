a03f0a6522a0b16ef048b3270b8e8af614e5fc86
Alex Trotman <alextrotman04@gmail.com>
2023-09-12 15:06:25 -0500

    finish

--- index.js
+++ index.js
@@ -14,11 +14,24 @@ function isPalindrome(word) {
 
   I will need to somehow iterate through the string, look at each character in two ways, from the beginning to end, and end to beginning, then compare the two and evaluate my value to be returned.
 
+  After closer examination, spliting the string into an array, reversing the order, and joining the array back together to make a string is a better solution.
+
 
 */
 
 /*
   Add written explanation of your solution here
+
+  Creates a 'reversedWord' constant variable and assigns the following:
+
+  1) word.split('')
+    - This takes the word passed in and changes it into an array
+
+  2) .reverse().join('')
+    - this reverses the array, and the joins it back together
+
+  3) return word === reversedWord;
+    - Compares the two versions of the word using strict comparasion
 */
 
 // You can run `node index.js` to view these console logs
